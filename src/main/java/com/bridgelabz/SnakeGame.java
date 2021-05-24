01160196dd6c4a24aa6e6a035ca80949d3770527
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-24 08:02:49 +0530

    Added : Initilition

---
+++ src/main/java/com/bridgelabz/SnakeGame.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeGame {
+        int position=0;
+        public static void main(String[] args){
+            System.out.println("Welcome To Snake And Ladder Game");
+        }
+
+}
