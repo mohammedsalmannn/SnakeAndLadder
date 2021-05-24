10c00faaf2f5456bd37e3cc699587f0ed98b8106
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-24 08:10:57 +0530

    Added : Rolling of Die

--- src/main/java/com/bridgelabz/SnakeGame.java
+++ src/main/java/com/bridgelabz/SnakeGame.java
@@ -1,9 +1,18 @@
 package com.bridgelabz;
 
-public class SnakeGame {
-        int position=0;
-        public static void main(String[] args){
-            System.out.println("Welcome To Snake And Ladder Game");
-        }
+import java.util.Random;
 
+public class SnakeGame {
+    int position=0;
+    public static void main(String[] args){
+        System.out.println("Welcome To Snake And Ladder Game");
+        SnakeGame snakeLadder = new SnakeGame();
+        snakeLadder.rollDice();
+    }
+    public int rollDice(){
+        Random random = new Random();
+        int dice = (random.nextInt(6)+1);
+        System.out.println("Die Roll Is :" + dice);
+        return dice;
+    }
 }
