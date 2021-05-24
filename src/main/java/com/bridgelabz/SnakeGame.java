e4adb0ac456add2c72be9a315e5134ed00cb00e7
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-24 08:19:41 +0530

    Added :Winning Position

--- src/main/java/com/bridgelabz/SnakeGame.java
+++ src/main/java/com/bridgelabz/SnakeGame.java
@@ -3,40 +3,46 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class SnakeGame {
-    int position=0;
-    public static void main(String[] args){
+    final static int WIN_POSITION = 100;
+    int position = 0;
+    int positionCheck = 0;
+
+    public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
         SnakeGame snakeLadder = new SnakeGame();
         snakeLadder.rollDice();
     }
-    public int rollDice(){
+
+    public int rollDice() {
         Random random = new Random();
-        int dice = (random.nextInt(6)+1);
+        int dice = (random.nextInt(6) + 1);
         System.out.println("Die Roll Is :" + dice);
         return dice;
     }
-    public void playerOption()  {
-        Random random = new Random();
-        int dice = rollDice();
-        int option = random.nextInt(3);
-        System.out.println("Player Option Is : " + option);
-        System.out.println("0.No Play 1.Ladder 2.Snake");
-        switch (option) {
 
-            case 0:
-                System.out.println("No Play");
-                break;
-            case 1:
-                System.out.println("Ladder");
-                position +=dice;
-                break;
-            case 2:
-                System.out.println("Snake");
-                position -=dice;
-                break;
-            default:
-                System.out.println("Error");
-                break;
+    public void playerOption() {
+        while (position < WIN_POSITION) {
+            Random random = new Random();
+            int dice = rollDice();
+            int option = random.nextInt(3);
+            System.out.println("Player Option Is : " + option);
+            switch (option) {
+
+                case 1:
+                    position += dice;
+                    System.out.println("Ladder Is At : " + position);
+                    break;
+                case 2:
+                    position -= dice;
+                    if ((position - dice) < positionCheck) {
+                        position = positionCheck;
+                    }
+                    System.out.println("Snake Is At : " + position);
+                    break;
+                default:
+                    System.out.println("No Play : " + position);
+                    break;
+            }
         }
     }
 }
